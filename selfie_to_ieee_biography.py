f1252d00d7c1b93075a4074874813e36d1bd6d81
Muhammad Yasirroni <48709672+yasirroni@users.noreply.github.com>
2022-01-19 20:12:38 +0700

    update for small image

--- selfie_to_ieee_biography.py
+++ selfie_to_ieee_biography.py
@@ -5,14 +5,27 @@ def selfie_to_ieee_biography(img_path, save=False, save_path=None, cascade_path=
     # Read the input image
     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
+    # Get img shape
+    h_img, w_img = img.shape
+
     # Get face
     x, y, w, h = get_face(img, cascade_path=cascade_path)
-    
-    # Expand face area
-    xmin, ymin, xmax, ymax = expand_face_area(x, y, w, h)
-    
-    # Crop and resize
-    img = crop_and_resize(img, xmin, ymin, xmax, ymax)
+    # print(w, h, w/h)
+
+    # Expand face
+    x_, y_, w_, h_ = expand_face_area(x, y, w, h, w_img, h_img)
+
+    # Resize whole img
+    r = 300 / w_ # or r = 375 / h
+    resize_dim = round(w_img * r), round(h_img * r)
+    img = cv2.resize(img, resize_dim, interpolation=cv2.INTER_AREA)
+
+    # Map x, y to resized img
+    x__ = round(x_ * r)
+    y__ = round(y_ * r)
+
+    # Crop
+    img = img[y__:y__+375,x__:x__+300]
     
     # Save cropped image
     if save:
@@ -38,24 +51,20 @@ def get_face(img, cascade_path=None):
     
     return x, y, w, h
 
-def expand_face_area(x, y, w, h):
-    # Expand face area
-    # TODO: no need to use xmid, ymid
-    xmid = x + w / 2
-    ymid = y + h / 2
+def expand_face_area(x, y, w, h, w_img, h_img):
+    # Shift x, y
+    x_ = max(0, x - 0.25 * w)
+    y_ = max(0, y - 0.425 * h)
 
-    xmin = int(xmid - 0.75 * w)
-    xmax = int(xmid + 0.75 * w)
-    ymin = int(ymid - 0.925 * h)
-    ymax = int(ymid + 0.950 * h)
-    
-    return xmin, ymin, xmax, ymax
+    # Expand w, h 
+    w_ = w * 1.5
+    h_ = h * 1.875
+    w__ = min(w_img - x_, w_)
+    h__ = min(h_img - y_, h_)
 
-def crop_and_resize(img, xmin, ymin, xmax, ymax, resize_dim=(300, 375)):
-    # crop and resize
-    img = cv2.resize(img[ymin:ymax,xmin:xmax], resize_dim, interpolation=cv2.INTER_AREA)    
-    
-    return img
+    bound_ratio = min(w__/w_, h__/h_)
+
+    return x_, y_, w * 1.5 * bound_ratio, h * 1.875 * bound_ratio
 
 def show_img(img):
     from matplotlib import pyplot as plt
